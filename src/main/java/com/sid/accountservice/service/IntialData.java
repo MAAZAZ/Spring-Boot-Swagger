4ea7ad8c42e1e980f015f27cb71f940e0d89666c
zmaazaz <zmaazaz@sqli.com>
2023-02-07 20:51:53 +0100

    use Spring profiles for bean

--- src/main/java/com/sid/accountservice/service/IntialData.java
+++ src/main/java/com/sid/accountservice/service/IntialData.java
@@ -4,13 +4,13 @@ import com.sid.accountservice.model.Account;
 import com.sid.accountservice.model.AccountType;
 import com.sid.accountservice.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Date;
 
+@Profile("dev")
 @Component
 public class IntialData {
     @Autowired
@@ -22,6 +22,5 @@ public class IntialData {
         accountRepository.save(new Account(null, Math.random() * 10000, new Date(), AccountType.EPARGNE));
         accountRepository.save(new Account(null, Math.random() * 10000, new Date(), AccountType.COURANT));
         accountRepository.save(new Account(null, Math.random() * 10000, new Date(), AccountType.EPARGNE));
-        accountRepository.findAll().forEach(System.out::println);
     }
 }
